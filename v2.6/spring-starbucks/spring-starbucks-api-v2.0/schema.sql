d2af55542ea7b58f8cdded29d8ec8ed20df7fe72
paulnguyen <paul.nguyen@sjsu.edu>
2022-03-01 23:19:19 -0800

    Initial Versions of Starbucks API Example

---
+++ v2.6/spring-starbucks/spring-starbucks-api-v2.0/schema.sql
@@ -0,0 +1,8 @@
+
+create database cmpe172 ;
+create user 'admin'@'%' identified by 'cmpe172'; 
+grant all on cmpe172.* to 'admin'@'%'; 
+
+use cmpe172 ;
+
+
---
+++ v2.6/spring-starbucks/starbucks-mobile-app/README.md
@@ -0,0 +1,5 @@
+# Starbucks Mobile App
+
+
+
+
