a99cf7d9bf0d45d52508c2ca12601899c46df95b
paulnguyen <paul.nguyen@sjsu.edu>
2022-04-20 21:16:51 -0700

    Update for Spring 2022

--- v2.6/spring-rabbitmq/spring-rabbitmq-helloworld/src/main/java/com/example/springrabbitmqhelloworld/SpringRabbitmqHelloworldApplication.java
+++ v2.6/spring-rabbitmq/spring-rabbitmq-helloworld/src/main/java/com/example/springrabbitmqhelloworld/SpringRabbitmqHelloworldApplication.java
@@ -3,11 +3,31 @@ package com.example.springrabbitmqhelloworld;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
+@EnableScheduling
 public class SpringRabbitmqHelloworldApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringRabbitmqHelloworldApplication.class, args);
+    @Profile("usage_message")
+    @Bean
+    public CommandLineRunner usage() {
+        return args -> {
+            System.out.println("This app uses Spring Profiles to control its behavior.\n");
+            System.out.println("Sample usage: java -jar spring-rabbitmq-helloworld-1.0.jar --spring.profiles.active=hello-world,sender|receiver");
+        };
     }
 
+    @Profile("!usage_message")
+    @Bean
+    public CommandLineRunner tutorial() {
+        return new RabbitAmqpTutorialsRunner();
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringRabbitmqHelloworldApplication.class, args);
+    }
 }
