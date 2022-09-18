3d3fccda40d5b54a4dbe6914d2b3d5015f1d7335
paulnguyen <paul.nguyen@sjsu.edu>
2022-09-18 15:53:58 -0700

    update

--- v2.6/spring-starbucks/spring-starbucks-client-v3.1/src/main/java/com/example/springstarbucksclient/ConsoleController.java
+++ v2.6/spring-starbucks/spring-starbucks-client-v3.1/src/main/java/com/example/springstarbucksclient/ConsoleController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
@@ -34,6 +35,10 @@ import java.util.Map;
 @RequestMapping("/")
 public class ConsoleController {
 
+    @Value("${starbucks.client.apikey}") String API_KEY ;
+    @Value("${starbucks.client.apihost}") String API_HOST ;
+    @Value("${starbucks.client.register}") String REGISTER ;
+
     @GetMapping
     public String getAction(@ModelAttribute("command") ConsoleCommand command,
                             Model model) {
@@ -53,16 +58,17 @@ public class ConsoleController {
         String message = "";
 
         // Set API Key Header
-        headers.set( "apikey", "2H3fONTa8ugl1IcVS7CjLPnPIS2Hp9dJ" ) ;
+        headers.set( "apikey", API_KEY ) ;
 
         if (action.equals("PING")) {
             message = "PING";
-            resourceUrl = "http://localhost:80/api/ping?apikey={apikey}";
+            resourceUrl = "http://"+API_HOST+"/ping?apikey="+API_KEY;
+            System.out.println(resourceUrl);
             // get response as string
-            ResponseEntity<String> stringResponse = restTemplate.getForEntity(resourceUrl, String.class, "2H3fONTa8ugl1IcVS7CjLPnPIS2Hp9dJ" );
+            ResponseEntity<String> stringResponse = restTemplate.getForEntity(resourceUrl, String.class, API_KEY );
             message = stringResponse.getBody();
             // get response as POJO
-            ResponseEntity<Ping> pingResponse = restTemplate.getForEntity(resourceUrl, Ping.class, "2H3fONTa8ugl1IcVS7CjLPnPIS2Hp9dJ");
+            ResponseEntity<Ping> pingResponse = restTemplate.getForEntity(resourceUrl, Ping.class, API_KEY);
             Ping pingMsg = pingResponse.getBody();
             System.out.println( pingMsg );
             // pretty print JSON
@@ -77,7 +83,7 @@ public class ConsoleController {
         }
         if (action.equals("NEW CARD")) {
             message = "";
-            resourceUrl = "http://localhost:80/api/cards";
+            resourceUrl = "http://"+API_HOST+"/cards";
             // get response as POJO
             String emptyRequest = "" ;
             HttpEntity<String> newCardRequest = new HttpEntity<String>(emptyRequest, headers) ;
@@ -95,7 +101,7 @@ public class ConsoleController {
         }
         if (action.equals("NEW ORDER")) {
             message = "";
-            resourceUrl = "http://localhost:80/api/order/register/5012349";
+            resourceUrl = "http://"+API_HOST+"/order/register/"+REGISTER;
             // get response as POJO
             Order orderRequest = new Order() ;
             orderRequest.setDrink("Caffe Latte") ;
@@ -116,7 +122,7 @@ public class ConsoleController {
         }
         if (action.equals("ACTIVATE CARD")) {
             message = "";
-            resourceUrl = "http://localhost:80/api/card/activate/"+command.getCardnum()+"/"+command.getCardcode();
+            resourceUrl = "http://"+API_HOST+"/card/activate/"+command.getCardnum()+"/"+command.getCardcode();
             // get response as POJO
             String emptyRequest = "" ;
             HttpEntity<String> newCardRequest = new HttpEntity<String>(emptyRequest,headers) ;
@@ -134,7 +140,7 @@ public class ConsoleController {
         }
         if (action.equals("PAY")) {
             message = "";
-            resourceUrl = "http://localhost:80/api/order/register/5012349/pay/"+command.getCardnum() ;
+            resourceUrl = "http://"+API_HOST+"/order/register/5012349/pay/"+command.getCardnum() ;
             System.out.println(resourceUrl) ;
             // get response as POJO
             String emptyRequest = "" ;
