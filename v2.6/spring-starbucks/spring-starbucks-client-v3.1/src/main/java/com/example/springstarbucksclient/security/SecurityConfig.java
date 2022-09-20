5268f9bd3f0bf6512a67d5a35faf86422c7aafc1
paulnguyen <paul.nguyen@sjsu.edu>
2022-09-19 22:47:02 -0700

    Update SecurityConfig.java

--- v2.6/spring-starbucks/spring-starbucks-client-v3.1/src/main/java/com/example/springstarbucksclient/security/SecurityConfig.java
+++ v2.6/spring-starbucks/spring-starbucks-client-v3.1/src/main/java/com/example/springstarbucksclient/security/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.example.springstarbucksclient.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,66 +15,62 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfig {
 
-
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll();
-    }
-
-
     /*
 
-    Autoconfiguring Spring Security
+        Autoconfiguring Spring Security
 
-    REF:  Securing Spring | Spring in Action, Sixth Edition
+        REF:  Securing Spring | Spring in Action, Sixth Edition
 
-    What does this barebones security configuration do for you? Not much, actually.
-    The main thing it does is declare a PasswordEncoder bean, which we’ll use both
-    when creating new users and when authenticating users at login.
+        What does this barebones security configuration do for you? Not much, actually.
+        The main thing it does is declare a PasswordEncoder bean, which we’ll use both
+        when creating new users and when authenticating users at login.
 
-    In this case, we’re using BCryptPasswordEncoder, one of a handful of password
-    encoders provided by Spring Security, including the following:
+        In this case, we’re using BCryptPasswordEncoder, one of a handful of password
+        encoders provided by Spring Security, including the following:
 
-        * BCryptPasswordEncoder—Applies bcrypt strong hashing encryption
-        * NoOpPasswordEncoder—Applies no encoding
-        * Pbkdf2PasswordEncoder—Applies PBKDF2 encryption
-        * SCryptPasswordEncoder—Applies Scrypt hashing encryption
-        * StandardPasswordEncoder—Applies SHA-256 hashing encryption
+            * BCryptPasswordEncoder—Applies bcrypt strong hashing encryption
+            * NoOpPasswordEncoder—Applies no encoding
+            * Pbkdf2PasswordEncoder—Applies PBKDF2 encryption
+            * SCryptPasswordEncoder—Applies Scrypt hashing encryption
+            * StandardPasswordEncoder—Applies SHA-256 hashing encryption
 
 
- */
+    */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
 
-
     /*
 
-    * https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/in-memory.html
-    * https://docs.spring.io/spring-boot/docs/current/reference/html/cli.html
+        * https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/in-memory.html
+        * https://docs.spring.io/spring-boot/docs/current/reference/html/cli.html
 
-    Spring Security’s InMemoryUserDetailsManager implements UserDetailsService to provide
-    support for username/password based authentication that is stored in memory.
-    InMemoryUserDetailsManager provides management of UserDetails by implementing
-    the UserDetailsManager interface. UserDetails based authentication is used by
-    Spring Security when it is configured to accept a username/password for authentication.
+        Spring Security’s InMemoryUserDetailsManager implements UserDetailsService to provide
+        support for username/password based authentication that is stored in memory.
+        InMemoryUserDetailsManager provides management of UserDetails by implementing
+        the UserDetailsManager interface. UserDetails based authentication is used by
+        Spring Security when it is configured to accept a username/password for authentication.
 
-    In this sample we use Spring Boot CLI to encode the password of password and get the
-    encoded password of {bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW.
+        In this sample we use Spring Boot CLI to encode the password of password and get the
+        encoded password of {bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW.
 
-    Spring Boot CLI:  spring encodepassword password
-    Alternatively:  https://bcrypt.online/
-
+        Spring Boot CLI:  spring encodepassword password
+        Alternatively:  https://bcrypt.online/
 
      */
     @Bean
@@ -86,6 +86,26 @@ public class SecurityConfig {
                 .roles("USER", "ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
+    /*
+        References:
+            * https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/form.html
+            * https://stackoverflow.com/questions/41373588/spring-security-configuration-for-post-request
+            * https://docs.spring.io/spring-security/reference/servlet/authorization/authorize-http-requests.html
+     */
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable(); // disable CSRF for POSTS
+        SecurityFilterChain ret = http
+                .formLogin(withDefaults())
+                .authorizeRequests()
+                .antMatchers("/console").hasRole("USER")
+                .antMatchers("/").permitAll()
+                .and()
+                .build() ;
+        return ret ;
     }
 
 }
